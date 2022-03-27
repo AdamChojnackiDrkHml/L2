e7989ac5747b984946362625f6f598d4c9b74c7f
Adam Chojnacki <adam.chojnacki.syl@gmail.com>
2022-03-27 17:31:46 +0200

    Basic algorith impl

--- cmd/coderMain/coderMain.go
+++ cmd/coderMain/coderMain.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"l2/pkg/coder"
 	"l2/pkg/reader"
 	"l2/pkg/writer"
+	"os"
 )
 
 func main() {
+	fmt.Println(os.Getwd())
 	reader := reader.Reader_createReader("data/input/test")
 	writer := writer.Writer_createReader("data/output/test")
 
--- pkg/coder/coder.go
+++ pkg/coder/coder.go
@@ -3,6 +3,7 @@ package coder
 import (
 	"l2/pkg/reader"
 	"l2/pkg/writer"
+	"strconv"
 
 	"github.com/shopspring/decimal"
 )
@@ -10,27 +11,28 @@ import (
 type Coder struct {
 	reader         *reader.Reader
 	writer         *writer.Writer
-	probs          []decimal.Decimal
+	probsF         []decimal.Decimal
 	counterSymbols []int64
 	iterations     int
 	currentPatch   []byte
 	lastPatch      bool
-	toWrite        string
+	tag            decimal.Decimal
 }
 
 func Coder_createCoder(reader *reader.Reader, writer *writer.Writer) *Coder {
 	coder := &Coder{reader: reader,
 		writer:         writer,
-		probs:          make([]decimal.Decimal, 256),
+		probsF:         make([]decimal.Decimal, 257),
 		counterSymbols: make([]int64, 256),
 		iterations:     0,
 		currentPatch:   make([]byte, 0),
 		lastPatch:      false}
 
-	for i := range coder.probs {
-		coder.probs[i] = decimal.NewFromInt(1).Div(decimal.NewFromInt(reader.PatchSize))
+	for i := range coder.probsF {
+		coder.probsF[i] = decimal.NewFromInt(1).Div(decimal.NewFromInt(reader.PatchSize)).Mul(decimal.NewFromInt(int64(i + 1)))
 	}
 
+	coder.probsF[len(coder.probsF)-1] = decimal.NewFromInt(1)
 	return coder
 }
 
@@ -44,8 +46,11 @@ func (coder *Coder) calcProbs() {
 
 	allSymbolsCounter := int64(iterations)*int64(coder.reader.PatchSize) + int64(coder.reader.ReadSymbolsCounter)
 
-	for i, n := range coder.counterSymbols {
-		coder.probs[i] = decimal.NewFromInt(n).Div(decimal.NewFromInt(allSymbolsCounter))
+	coder.probsF[0] = decimal.NewFromInt(coder.counterSymbols[0]).Div(decimal.NewFromInt(allSymbolsCounter))
+
+	for i := 1; i < len(coder.counterSymbols); i++ {
+		temp := decimal.NewFromInt(coder.counterSymbols[i]).Div(decimal.NewFromInt(allSymbolsCounter))
+		coder.probsF[i] = coder.probsF[i-1].Add(temp)
 	}
 
 	coder.iterations++
@@ -62,11 +67,29 @@ func (coder *Coder) prepareAllDataToCoding() {
 }
 
 func (coder *Coder) writeCode() {
-	coder.writer.Writer_writeToFile(coder.toWrite)
+	coder.writer.Writer_writeToFile(strconv.Itoa(len(coder.currentPatch)) + " " + coder.tag.String() + "\n")
 }
 
 func (coder *Coder) code() {
-	coder.toWrite = string(coder.currentPatch)
+
+	l := decimal.NewFromInt(0)
+	p := decimal.NewFromInt(1)
+
+	for _, n := range coder.currentPatch {
+		d := p.Sub(l)
+		p = l.Add(coder.probsF[n+1].Mul(d))
+		l = coder.probsF[n].Mul(d)
+
+		for p.LessThan(decimal.NewFromFloat(0.5)) {
+			l = l.Mul(decimal.NewFromInt(2))
+			p = p.Mul(decimal.NewFromInt(2))
+		}
+	}
+	// fmt.Println(l.String())
+	// fmt.Println(p.String())
+	coder.tag = l.Add(p)
+	coder.tag = coder.tag.Div(decimal.NewFromFloat(2.0))
+
 }
 
 func (coder *Coder) Coder_run() {
