254117cf6063808c9b740ce7b3052dd9eb6ef71a
AdamChojnackiLaptop <adam.chojnacki.syl@gmail.com>
2022-03-31 10:59:54 +0200

    big refactor and improve a lot

--- pkg/coder/coder.go
+++ pkg/coder/coder.go
@@ -1,44 +1,47 @@
 package coder
 
 import (
+	"fmt"
 	"l2/pkg/reader"
 	"l2/pkg/writer"
 	"strconv"
-
-	"github.com/shopspring/decimal"
 )
 
 type Coder struct {
 	reader         *reader.Reader
 	writer         *writer.Writer
-	probsF         []decimal.Decimal
+	probsF         []float64
 	counterSymbols []int64
 	iterations     int
 	currentPatch   []byte
 	lastPatch      bool
-	tag            decimal.Decimal
+	tag            float64
 	w              string
+	bitBuffer      []byte
+	bytesBuffer    []byte
 }
 
 func Coder_createCoder(reader *reader.Reader, writer *writer.Writer) *Coder {
 	coder := &Coder{reader: reader,
 		writer:         writer,
-		probsF:         make([]decimal.Decimal, 257),
+		probsF:         make([]float64, 257),
 		counterSymbols: make([]int64, 256),
 		iterations:     0,
 		currentPatch:   make([]byte, 0),
-		lastPatch:      false}
+		lastPatch:      false,
+		bitBuffer:      make([]byte, 0),
+		bytesBuffer:    make([]byte, 0)}
 
 	for i := range coder.counterSymbols {
 		coder.counterSymbols[i] = 1
 	}
 
+	singleProb := 1.0 / 256.0
 	for i := range coder.probsF {
-		coder.probsF[i] = decimal.NewFromInt(1).Div(decimal.NewFromInt(256)).Mul(decimal.NewFromInt(int64(i)))
-		coder.w = coder.probsF[i].String()
+		coder.probsF[i] = singleProb * float64(i)
+		coder.w = fmt.Sprintf("%f", coder.probsF[i])
 	}
 
-	coder.probsF[len(coder.probsF)-1] = decimal.NewFromInt(1)
 	return coder
 }
 
@@ -52,11 +55,9 @@ func (coder *Coder) calcProbs() {
 
 	allSymbolsCounter := int64(iterations)*int64(coder.reader.PatchSize) + int64(coder.reader.ReadSymbolsCounter) + 256
 
-	coder.probsF[0] = decimal.NewFromInt(coder.counterSymbols[0]).Div(decimal.NewFromInt(allSymbolsCounter))
-
-	for i := 1; i < len(coder.counterSymbols); i++ {
-		temp := decimal.NewFromInt(coder.counterSymbols[i]).Div(decimal.NewFromInt(allSymbolsCounter))
-		coder.probsF[i] = coder.probsF[i-1].Add(temp)
+	for i := 0; i < len(coder.counterSymbols); i++ {
+		temp := float64(coder.counterSymbols[i]) / float64(allSymbolsCounter)
+		coder.probsF[i] = coder.probsF[i-1] + temp
 		//fmt.Println(coder.probsF[i].String())
 	}
 
@@ -69,89 +70,117 @@ func (coder *Coder) getData() {
 }
 
 func (coder *Coder) writeCode() {
-	coder.writer.Writer_writeToFile(strconv.Itoa(len(coder.currentPatch)) + " " + coder.tag.String() + "\n")
+	coder.writer.Writer_writeToFile(strconv.Itoa(len(coder.currentPatch)) + " " + fmt.Sprintf("%f", coder.tag) + "\n")
+}
+
+func (coder *Coder) writeBytesBuffer() {
+	coder.writer.Writer_writeToFile(coder.w)
 }
 
 func (coder *Coder) code() {
 
-	l := decimal.NewFromInt(0)
-	p := decimal.NewFromInt(1)
-	prefix := make([]int64, 0)
+	l := 0.0
+	p := 1.0
 	counter := 0
 
 	for _, n := range coder.currentPatch {
-		d := p.Sub(l)
+		d := p - l
 		//fmt.Println(d.String())
 		//fmt.Println(coder.probsF[n+1])
-		p = l.Add(coder.probsF[n+1].Mul(d))
+		p = l + (coder.probsF[n+1] * d)
 		//fmt.Println(p.String())
 
-		l = l.Add(coder.probsF[n].Mul(d))
+		l = l + (coder.probsF[n] * d)
 		//fmt.Println(l.String())
-		s := l.String()
-		s = coder.probsF[n].String()
-		for p.LessThan(decimal.NewFromFloat(0.5)) {
-			l = l.Mul(decimal.NewFromInt(2))
-			p = p.Mul(decimal.NewFromInt(2))
-			//fmt.Println(p.String())
-			//fmt.Println(l.String())
+		s := fmt.Sprintf("%f", l)
+		s = fmt.Sprintf("%f", coder.probsF[n])
+		for {
+			if p < 0.5 {
+				l = l * 2.0
+				p = p * 2.0
 
-			for counter > 0 {
-				prefix = append(prefix, 1)
-				counter--
+				coder.addToBuffer(0)
+				for counter > 0 {
+					coder.addToBuffer(1)
+					counter--
+				}
+
+			} else if l >= 0.5 {
+				l = (l * 2.0) - 1.0
+				s = fmt.Sprintf("%f", l)
+				p = (p * 2.0) - 1.0
+				coder.addToBuffer(1)
+				for counter > 0 {
+					coder.addToBuffer(0)
+					counter--
+				}
+				coder.w = s
+			} else if caseThreeCondtionCheck(l, p) {
+				l = (l * 2.0) - 0.5
+				p = (p * 2.0) - 0.5
+				counter++
+			} else {
+				break
 			}
-			prefix = append(prefix, 0)
-		}
-		s = l.String()
-		for decimal.NewFromFloat(0.5).LessThanOrEqual(l) {
-			l = l.Mul(decimal.NewFromInt(2)).Sub(decimal.NewFromInt(1))
-			s = l.String()
-			p = p.Mul(decimal.NewFromInt(2)).Sub(decimal.NewFromInt(1))
-
-			for counter > 0 {
-				prefix = append(prefix, 0)
-				counter--
-			}
-			prefix = append(prefix, 1)
-			coder.w = s
-		}
-
-		for caseThreeCondtionCheck(l, p) {
-			l = l.Mul(decimal.NewFromInt(2)).Sub(decimal.NewFromFloat(0.5))
-			p = p.Mul(decimal.NewFromInt(2)).Sub(decimal.NewFromFloat(0.5))
-			counter++
 		}
 	}
+	temp := p - l
+	temp = temp / 2
 
-	coder.tag = l.Add(p)
-	coder.tag = coder.tag.Mul(decimal.NewFromFloat(0.5))
+	coder.tag = l + temp
 
-	coder.scaleTag(prefix)
 }
 
-func (coder *Coder) scaleTag(bits []int64) {
+func (coder *Coder) addToBuffer(bit byte) {
+	coder.bitBuffer = append(coder.bitBuffer, bit)
 
-	for _, n := range bits {
-		coder.tag = coder.tag.Add(decimal.NewFromInt(1 * n))
-		coder.tag = coder.tag.Mul(decimal.NewFromFloat(0.5))
-
+	if len(coder.bitBuffer) == 256 {
+		coder.writeBits()
+		coder.bitBuffer = make([]byte, 0)
 	}
 }
 
-func caseThreeCondtionCheck(l, p decimal.Decimal) bool {
-	if p.LessThanOrEqual(decimal.NewFromFloat(0.5)) {
+func (coder *Coder) writeBits() {
+	for len(coder.bitBuffer) > 0 {
+		myByteBits := coder.bitBuffer[:8]
+		coder.bitBuffer = coder.bitBuffer[8:]
+
+		acc := byte(0)
+
+		for _, n := range myByteBits {
+			acc *= 2
+			acc += n
+		}
+
+		coder.addByteToBuffer(acc)
+	}
+}
+
+func (coder *Coder) addByteToBuffer(myByte byte) {
+
+	coder.bytesBuffer = append(coder.bytesBuffer, myByte)
+
+	if len(coder.bytesBuffer) == 256 {
+		coder.w = string(coder.bytesBuffer)
+		coder.writeBytesBuffer()
+		coder.bytesBuffer = make([]byte, 0)
+	}
+}
+
+func caseThreeCondtionCheck(l, p float64) bool {
+	if p <= 0.5 {
 		return false
 	}
 
-	if decimal.NewFromFloat(0.5).LessThanOrEqual(l) {
+	if l >= 0.5 {
 		return false
 	}
 
-	if decimal.NewFromFloat(0.75).LessThanOrEqual(p) {
+	if p >= 0.75 {
 		return false
 	}
 
-	if l.LessThan(decimal.NewFromFloat(0.25)) {
+	if l < 0.25 {
 		return false
 	}
 
